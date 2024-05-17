84e90e6c07c75fbe565eaa4e3050def5ee44fe23
Filip Petrovic <filippetrovicfr04@gmail.com>
2024-05-17 16:32:34 +0200

    Suppression de la composante Gallerie

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import styles from "./Collapse.module.scss";
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid'
 
--- src/pages/Accueil/Page.jsx
+++ src/pages/Accueil/Page.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styles from "./Page.module.scss";
 import homeImg from "../../images/home-img.webp";
 import data from "../../data.json";
-import Gallery from '../../components/Gallery/Gallery';
+import Card from '../../components/Card/Card';
 
 const Page = () => {
 
@@ -22,12 +22,19 @@ const Page = () => {
   return (
     <main className={styles.main}>
       <div className={styles.imgContainer}>
-        <img src={homeImg} alt="L'ocean et les montmagnes"/>
+        <img src={homeImg} alt="L'ocean et les montmagnes" />
         <h1>Chez vous,{windowWidth < 750 && <br />} partout et ailleurs</h1>
       </div>
-      <Gallery 
-        data = {data}
-      />
+      <div className={styles.gallery}>{data.map((item) => {
+        return (
+          <Card
+            key={item.id}
+            id={item.id}
+            cover={item.cover}
+            title={item.title}
+          />
+        )
+      })}</div>
     </main>
   )
 }
